56c860ae3b99df1d1fb08ed3dc1d6cb8ed182af4
jumymd <fifaka1000@gmail.com>
2020-10-18 12:17:24 +0900

    first commit

---
+++ imitate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imitate : MonoBehaviour
+{
+    private Renderer targetRend;
+    private Renderer myRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRend = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Bcube")
+        {
+            Debug.Log("catchB");
+            myRend.enabled = true;
+            targetRend = collider.GetComponent<Renderer>();
+            myRend.material = targetRend.material;
+        }
+    }
+}
---
+++ move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        PressX();
+    }
+
+    void PressX()
+    {
+        if (Input.GetKeyDown(KeyCode.X)){
+            Debug.Log("keyDownX");
+            transform.position = new Vector3(transform.position.x, 0.05f, transform.position.z);
+        }
+    }
+
+}
